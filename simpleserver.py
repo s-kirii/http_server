621862f405fb7c1eb8e783818488f4386127f5f4
s-kirii <physics-sf@hotmail.co.jp>
2021-06-19 21:17:27 +0900

    add function add_ip_addresses

--- simpleserver.py
+++ simpleserver.py
@@ -29,40 +29,46 @@ class HTTPServersHandle():
         self.ipv4 = ipv4
         self.Handler = SimpleHTTPRequestHandler
 
+        self.ip_list6 = []
+        self.ip_list4 = []
+
         if self.auto:
             if not ipv6 and not ipv4:
                 raise Exception("If you set auto mode you mast set True in ipv6 or ipv4")
             ip_list = subprocess.getoutput("hostname -I").split(" ")
             ip_list = [d for d in ip_list if d != ""]
             if ipv6:
-                self.ip_list6 = [d for d in ip_list if isinstance(ip_address(d), IPv6Address) and not d in self.blacklist]
+                for d in ip_list:
+                    if isinstance(ip_address(d), IPv6Address) and not d in self.blacklist:
+                        self.ip_list6.append(d)
                 if len(self.ip_list6) == 0:
                     raise Exception("Can't get any IPv6 address")
                 else:
                     logger.info("Detect IPv6 address list %s"%(self.ip_list6))
 
             if ipv4:
-                self.ip_list4 = [d for d in ip_list if isinstance(ip_address(d), IPv4Address) and not d in self.blacklist]
+                for d in ip_list:
+                    if isinstance(ip_address(d), IPv4Address) and not d in self.blacklist:
+                        self.ip_list4.append(d)
                 if len(self.ip_list4) == 0:
                      raise Exception("Can't get any IPv6 address")
                 else:
                     logger.info("Detect IPv4 address list %s"%(self.ip_list4))
+        else:
+            logger.info("You must set IP address manually")
 
     def start_server(self, ipv6 = False, addr=None):
         if addr is None:
-            if self.auto:
-                if ipv6:
-                    if self.ipv6:
-                        addr = self.ip_list6[0], self.port
-                    else:
-                        raise Exception("Cant't set IPv6.")
+            if ipv6:
+                if len(self.ip_list6) != 0:
+                    addr = self.ip_list6[0], self.port
                 else:
-                    if self.ipv4:
-                        addr = self.ip_list4[0], self.port
-                    else:
-                        raise Exception("Cant't set IPv4.")
+                    raise Exception("Cant't set IPv6.")
             else:
-                raise Exception("Must set addr.")
+                if len(self.ip_list4) != 0:
+                    addr = self.ip_list4[0], self.port
+                else:
+                    raise Exception("Cant't set IPv4.")
 
         server = TCPServer6 if ipv6 else TCPServer
         with server(addr, self.Handler) as httpd:
@@ -76,8 +82,21 @@ class HTTPServersHandle():
                 del httpd
                 logger.info("stop server")
 
-    def start_servers():
-        pass
+    def add_ip_addresses(self, ip):
+        try:
+            if isinstance(ip_address(ip), IPv6Address):
+                self.ip_list6.append(ip)
+                logger.info("Set IPv6 address")
+            elif isinstance(ip_address(ip), IPv4Address):
+                self.ip_list4.append(ip)
+                logger.info("Set IPv4 Address")
+            else:
+                raise Exception("IP address format what you set is incorrect.")
+        except ValueError:
+            raise Exception("IP address format what you set is incorrect.")
+
+    def get_ip_lists(self):
+        return {"IPv4": self.ip_list4, "IPv6": self.ip_list6}
 
 if __name__ == "__main__":
     ipv6 = False
