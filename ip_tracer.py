fb96878884cde8cda05292230258d6b5a9fc68b5
s-kirii <physics-sf@hotmail.co.jp>
2023-08-08 19:55:54 +0900

    socketを用いたIPアドレス取得方式に変更。ip_tracerというAPI上で実現。

---
+++ ip_tracer.py
@@ -0,0 +1,10 @@
+import socket
+
+def get_ip():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(("8.8.8.8", 80))
+    return s.getsockname()[0]
+
+if __name__ == "__main__":
+    ip = get_ip()
+    print(ip)
